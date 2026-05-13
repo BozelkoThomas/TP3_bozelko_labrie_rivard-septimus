88f9b44c54b5b83e48d5d268297a6b2aafd76fda
Thomas Bozelko <2549958@cmontmorency.qc.ca>
2026-05-13 14:14:18 -0400

    cartes test

--- pizza.js
+++ pizza.js
@@ -1,5 +1,13 @@
 // Cartes
 
+const carte = document.getElementsByClassName("pizzas__cards");
+const form = document.createElement("form");
+const titre = document.createElement("h2");
+
+titre.textContent = "La pizza de Freddy";
+form.appendChild(titre);
+carte[0].appendChild(form);
+
 // Mode de paiement et informations sur le client
 
 // Informations pour la livraison
