b908efda550195168f17c8b76a035aa8977b0e95
Anne-Julie Labrie <2509704@cmontmorency.qc.ca>
2026-05-13 16:32:59 -0400

    facture

--- pizza.js
+++ pizza.js
@@ -93,6 +93,7 @@ const titreFacture = document.createElement("h2");
 titreFacture.textContent = "Pizzeria Freddy Fazbear"
 facture.appendChild(titreFacture);
 
+
 // Commande
 const commande = document.getElementById("facture-commande");
 const titreCommande = document.createElement("div");
@@ -102,6 +103,18 @@ facture.appendChild(titreCommande);
 const pizza1Container = document.createElement("div");
 pizza1Container.setAttribute("id", "pizza1Container");
 facture.appendChild(pizza1Container);
+
+const pizza2Container = document.createElement("div");
+pizza2Container.setAttribute("id", "pizza2Container");
+facture.appendChild(pizza2Container);
+
+const pizza3Container = document.createElement("div");
+pizza3Container.setAttribute("id", "pizza3Container");
+facture.appendChild(pizza3Container);
+
+const pizza4Container = document.createElement("div");
+pizza4Container.setAttribute("id", "pizza4Container");
+facture.appendChild(pizza4Container);
 
 const titreCommandeNom = document.createElement("h3");
 titreCommandeNom.textContent = "Détails de la commande"
@@ -114,9 +127,9 @@ titreCommande.appendChild(titreCommandeQt);
 const titreCommandePrix = document.createElement("h3");
 titreCommandePrix.textContent = "Prix"
 titreCommande.appendChild(titreCommandePrix);
-
+// Freddy
 const pizza1 = document.createElement("p");
-pizza1.textContent = "Détails de la commande"
+pizza1.textContent = "Pizza Freddy"
 pizza1Container.appendChild(pizza1);
 
 const qt1 = document.createElement("input");
@@ -126,4 +139,66 @@ pizza1Container.appendChild(qt1);
 const prix1 = document.createElement("input");
 prix1.setAttribute("type", "number")
 pizza1Container.appendChild(prix1);
+// Bonnie
+const pizza2 = document.createElement("p");
+pizza2.textContent = "Pizza Bonnie"
+pizza2Container.appendChild(pizza2);
 
+const qt2 = document.createElement("input");
+qt2.setAttribute("type", "number")
+pizza2Container.appendChild(qt2);
+
+const prix2 = document.createElement("input");
+prix2.setAttribute("type", "number")
+pizza2Container.appendChild(prix2);
+// Chica
+const pizza3 = document.createElement("p");
+pizza3.textContent = "Pizza Chica"
+pizza3Container.appendChild(pizza3);
+
+const qt3 = document.createElement("input");
+qt3.setAttribute("type", "number")
+pizza3Container.appendChild(qt3);
+
+const prix3 = document.createElement("input");
+prix3.setAttribute("type", "number")
+pizza3Container.appendChild(prix3);
+// Foxy
+const pizza4 = document.createElement("p");
+pizza4.textContent = "Pizza Chica"
+pizza4Container.appendChild(pizza4);
+
+const qt4 = document.createElement("input");
+qt4.setAttribute("type", "number")
+pizza4Container.appendChild(qt4);
+
+const prix4 = document.createElement("input");
+prix4.setAttribute("type", "number")
+pizza4Container.appendChild(prix4);
+
+
+// Client
+const client = document.getElementById("facture-client");
+const titreClient = document.createElement("h3");
+titreClient.textContent = "Informations sur le client"
+facture.appendChild(titreClient);
+
+const clientContainer = document.createElement("div");
+clientContainer.setAttribute("id", "clientContainer");
+facture.appendChild(clientContainer);
+
+const nomPrenom = document.createElement("input");
+nomPrenom.setAttribute("type", "text");
+clientContainer.appendChild(nomPrenom);
+
+const telephone = document.createElement("input");
+telephone.setAttribute("type", "tel");
+clientContainer.appendChild(telephone);
+
+const courriel = document.createElement("input");
+courriel.setAttribute("type", "email");
+clientContainer.appendChild(courriel);
+
+const adresse = document.createElement("input");
+adresse.setAttribute("type", "text");
+clientContainer.appendChild(adresse);
