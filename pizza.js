f1ac972a982459906202a1940b9c530c10d269fa
Anne-Julie Labrie <2509704@cmontmorency.qc.ca>
2026-05-13 14:50:00 -0400

    meow

--- pizza.js
+++ pizza.js
@@ -33,41 +33,20 @@ paiement.appendChild(titrePaiement);
 
 // Informations pour la livraison
 
-// const livraison = document.getElementById("livraison");
-// const titreLivraison = document.createElement("h3");
-// titreLivraison.textContent = "Informations pour la livraison";
-// livraison.appendChild(titreLivraison);
+const livraison = document.getElementById("livraison");
+const titreLivraison = document.createElement("h3");
+titreLivraison.textContent = "Informations pour la livraison";
+livraison.appendChild(titreLivraison);
 
-// // Création du label
+const labelNom = document.createElement("label");
+labelNom.textContent = "Nom et prénom";
+const inputNom = document.createElement("input");
+inputNom.setAttribute("type", "text");
 
-// const labelNom = document.createElement("label");
+inputNom.setAttribute("placeholder", "Nom et prénom");
+inputNom.setAttribute("id", "nomLivraison");
+livraison.appendChild(labelNom);
 
-// // Texte du label
-// labelNom.textContent = "Nom et prénom";
-
-// // Création du input
-
-// const inputNom = document.createElement("input");
-
-// // Type du input = texte
-// inputNom.setAttribute("type", "text");
-
-// // Texte gris affiché dans le champ
-// inputNom.setAttribute("placeholder", "Nom et prénom");
-
-// // ID unique pour pouvoir récupérer le input plus tard
-
-// inputNom.setAttribute("id", "nomLivraison");
-
-// // On ajoute le label dans le div livraison
-
-// livraison.appendChild(labelNom);
-
-// // On ajoute le input dans le div livraison
-
-// livraison.appendChild(inputNom);
-
-// // On ajoute un saut de ligne
 
 
 
