7c763c2d17610676697ace90a600eca5d5eda9e6
Thomas Bozelko <2549958@cmontmorency.qc.ca>
2026-05-14 18:02:22 -0400

    p

--- pizza.js
+++ pizza.js
@@ -59,7 +59,7 @@ while (i < nomsPizzas.length) {
     btnMoinsPizza.textContent = "-";
     conteneurQtyPizza.appendChild(btnMoinsPizza);
 
-    const qtyPizza = document.createElement("input");
+    const qtyPizza = document.createElement("p");
     qtyPizza.setAttribute("id", "qtyPizza" + (i + 1));
     qtyPizza.setAttribute("class", "qtyPizza");
     qtyPizza.setAttribute("type", "number" + (i + 1));
